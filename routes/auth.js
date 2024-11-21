284848614e961d42fdd788b9c4cba9048a0cb38a
FaizUrRehman <shadowwalker3600@gmail.com>
2024-11-21 12:24:10 +0500

    add try catch and send user to db

--- routes/auth.js
+++ routes/auth.js
@@ -5,18 +5,25 @@ const { body, validationResult } = require('express-validator');
 
 
 
-
-router.post('/',[
+// Create a user using: POST "/api/auth/createuser". No login required
+router.post('/createuser',[
     body('name', 'Enter a valid name').isLength({min: 3}),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
 
-] , (req,res)=>{
+] , async (req,res)=>{
+    // If there are errors, return bad requests and that errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({errors: errors.array()});
     }
-    User.create({
+    // Chech whether the user with this email exists already
+    let user = await User.findOne({email: req.body.email})
+    if(user){
+        return res.status(400).json({error: "Sorry the user with this email already exists"})
+    }
+    try{
+     user = await User.create({
     name: req.body.name,
         email: req.body.email,
         password:req.body.password,
@@ -26,7 +33,13 @@ router.post('/',[
         province:req.body.province,
         district:req.body.district,
         nationality:req.body.nationality
-    }).then(user => res.json(user))
-    .catch(err=>{res.json({error: 'Please enter a unique value for email'})})
+    })
+    // send user in mongodb
+    res.json(user)
+    // If error occured in server the show the status 500
+} catch(error){
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
 });
 module.exports=router
